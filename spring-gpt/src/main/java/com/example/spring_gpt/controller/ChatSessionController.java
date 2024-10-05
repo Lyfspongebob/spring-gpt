1f22e77eb62c519b655661d5656e77a70c30d24a
Lyfspongebob <3080291974@qq.com>
2024-10-05 09:06:42 +0800

    v-1.0

--- spring-gpt/src/main/java/com/example/spring_gpt/controller/ChatSessionController.java
+++ spring-gpt/src/main/java/com/example/spring_gpt/controller/ChatSessionController.java
@@ -6,24 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @CrossOrigin
 @Controller
 @RequestMapping("/session")
 public class ChatSessionController {
+
     @Autowired
     private ChatSessionRepository chatSessionRepository;
 
-    @GetMapping("/get")     //session/get
-    public @ResponseBody ChatSession getSessionById(@RequestParam String id){
-        Optional<ChatSession> session = chatSessionRepository.findById(id);
-        if(session.isPresent()){
-            return session.get();
-        }else{
-            return null;
-        }
-    }
+//    @GetMapping("/get")     //session/get
+//    public @ResponseBody ChatSession getSessionById(@RequestParam String id){
+//        Optional<ChatSession> session = chatSessionRepository.findById(id);
+//        if(session.isPresent()){
+//            return session.get();
+//        }else{
+//            return null;
+//        }
+//    }
 
-    
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<ChatSession> getAllSessions() {
+        // This returns a JSON or XML with the users
+        return chatSessionRepository.findAll();
+    }
 }
